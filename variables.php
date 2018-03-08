1a0e90b142b135a21843005527f57e181313ca45
Dariush Mohammadi <Dariush@roomavailability.info>
2018-03-08 22:27:51 +0100

    change start date

--- variables.php
+++ variables.php
@@ -1,7 +1,7 @@
 <?php
 
 $url 		= 'http://www.mocky.io/v2/58ff37f2110000070cf5ff16';
-$startTime 	= '2017-10-31T11:59';
+$startTime 	= '2017-10-30T11:59';
 $endTime 	= '2017-11-30T11:59';
 $fileDir 	= __DIR__ . '/public/csv/file.csv';
 
